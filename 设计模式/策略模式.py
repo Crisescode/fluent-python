8d84f02233907e6f33f45c5ff8766d9cb84d08d9
Crisescode <zhaopanp2018@outlook.com>
2020-07-01 16:50:33 +0800

    add 策略模式

--- 设计模式/策略模式.py
+++ 设计模式/策略模式.py
@@ -5,66 +5,10 @@ from collections import defaultdict
 from abc import ABC, abstractmethod
 
 
-class Cashier:
-    def __init__(self):
-        self.commoditys = defaultdict(float)
-
-    def convert_str(self, string):
-        if "." in string:
-            return float(string)
-        else:
-            return int(string)
-
-    def counter(self, commodity, price, number):
-        single_price = self.convert_str(price) * self.convert_str(number)
-        self.commoditys[commodity] = single_price
-        self.commoditys["total"] += single_price
-        return self.commoditys
-
-
-if __name__ == "__main__":
-    commodity = input("please enter items separated by commas: ")
-    price = input("please enter price for every item separated by commas: ")
-    number = input("please enter number for every item separated by commas: ")
-
-    print(commodity.split(','))
-    print(price.split(','))
-    print(number.split(','))
-
-    cashier = Cashier()
-
-    for i in range(len(commodity.split(","))):
-        commodity_display = cashier.counter(
-            commodity.split(',')[i], price.split(",")[i], number.split(",")[i]
-        )
-
-    print(commodity_display)
-
-
-# ------------------------------------------------------------------------------
-
-
 # 简单工厂模式
-class Cashier2:
-    def __init__(self):
-        self.commoditys = defaultdict(float)
-
-    def convert_str(self, string):
-        if "." in string:
-            return float(string)
-        else:
-            return int(string)
-
-    def counter(self, commodity, price, number):
-        single_price = self.convert_str(price) * self.convert_str(number)
-        self.commoditys[commodity] = single_price
-        self.commoditys["total"] += single_price
-        return self.commoditys
-
-
 class Promotion(ABC):
     @abstractmethod
-    def discount(self, order):
+    def discount(self, money):
         pass
 
 
@@ -73,18 +17,101 @@ class NormalPromotion(Promotion):
         return money
 
 
-class DisPromotion(Promotion):
-    def discount(self, money, discount):
-        return money * discount
+class DisCountPromotion(Promotion):
+    def __init__(self, discount):
+        self._discount = discount
+
+    def discount(self, money):
+        return money * self._discount
 
 
 class ReducePromotion(Promotion):
-    def discount(self, order):
-        pass
+    def __init__(self, reduction):
+        self._reduction = reduction
+
+    def discount(self, money):
+        return money - self._reduction
 
 
 class PromotionFactory:
-    pass
+    promotion = dict()
+    promotion["discount"] = DisCountPromotion
+    promotion["reduction"] = ReducePromotion
+
+    def createPro(self, pro=None):
+        if pro in self.promotion:
+            return self.promotion[pro]
+        else:
+            return NormalPromotion
+
+
+# if __name__ == "__main__":
+#     commodity = input("please enter items separated by commas: ")
+#     price = input("please enter price for every item separated by commas: ")
+#     number = input("please enter number for every item separated by commas: ")
+#
+#     commoditys = defaultdict(float)
+#
+#     for i in range(len(commodity.split(","))):
+#         singe_price = int(price.split(",")[i]) * int(number.split(",")[i])
+#         commoditys[commodity.split(",")[i]] = singe_price
+#         commoditys["total"] += singe_price
+#
+#     if commoditys["total"] > 5000:
+#         pro = PromotionFactory().createPro("discount")
+#         commoditys["total"] = pro(0.8).discount(commoditys["total"])
+#         print(commoditys)
+#     elif 1000 < commoditys["total"] < 5000:
+#         pro = PromotionFactory().createPro("reduction")
+#         commoditys["total"] = pro(400).discount(commoditys["total"])
+#         print(commoditys)
+#     else:
+#         pro = PromotionFactory().createPro()
+#         commoditys["total"] = pro().discount(commoditys["total"])
+#         print(commoditys)
+
+
+# ------------------------------------------------------------------------------
+
+
+# 策略模式
+class Promotion2(ABC):
+    @abstractmethod
+    def discount(self, money):
+        pass
+
+
+class NormalPromotionSt(Promotion2):
+    def discount(self, money):
+        return money
+
+
+class DisCountPromotionSt(Promotion2):
+    def __init__(self, discount):
+        self._discount = discount
+
+    def discount(self, money):
+        return money * self._discount
+
+
+class ReducePromotionSt(Promotion2):
+    def __init__(self, reduction):
+        self._reduction = reduction
+
+    def discount(self, money):
+        return money - self._reduction
+
+
+class Promotion2Factory:
+    promotion = dict()
+    promotion["discount"] = DisCountPromotion
+    promotion["reduction"] = ReducePromotion
+
+    def createPro(self, pro=None):
+        if pro in self.promotion:
+            return self.promotion[pro]
+        else:
+            return NormalPromotion
 
 
 if __name__ == "__main__":
@@ -92,3 +119,22 @@ if __name__ == "__main__":
     price = input("please enter price for every item separated by commas: ")
     number = input("please enter number for every item separated by commas: ")
 
+    commoditys = defaultdict(float)
+
+    for i in range(len(commodity.split(","))):
+        singe_price = int(price.split(",")[i]) * int(number.split(",")[i])
+        commoditys[commodity.split(",")[i]] = singe_price
+        commoditys["total"] += singe_price
+
+    if commoditys["total"] > 5000:
+        pro = PromotionFactory().createPro("discount")
+        commoditys["total"] = pro(0.8).discount(commoditys["total"])
+        print(commoditys)
+    elif 1000 < commoditys["total"] < 5000:
+        pro = PromotionFactory().createPro("reduction")
+        commoditys["total"] = pro(400).discount(commoditys["total"])
+        print(commoditys)
+    else:
+        pro = PromotionFactory().createPro()
+        commoditys["total"] = pro().discount(commoditys["total"])
+        print(commoditys)
